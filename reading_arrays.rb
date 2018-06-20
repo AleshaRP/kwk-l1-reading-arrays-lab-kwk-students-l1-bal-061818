e94b77b18b6503cf0e840f8207ef059c835fd7a8
AleshaRP <createlele@yahoo.com>
2018-06-20 15:53:53 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -13,12 +13,12 @@ end
 first_student_by_index
 
 def fourth_student_by_index
-  STUDENT_NAMES[4]
+  STUDENT_NAMES[3]
 end
 fourth_student_by_index
 
 def last_student_by_index
-  STUDENT_NAMES[6]
+  STUDENT_NAMES[5]
 end
 last_student_by_index
 
@@ -33,8 +33,8 @@ end
 last_student_by_method
 
 def first_second_and_third_students
+  STUDENT_NAMES[0]
   STUDENT_NAMES[1]
   STUDENT_NAMES[2]
-  STUDENT_NAMES[3]
 end
 first_second_and_third_students
